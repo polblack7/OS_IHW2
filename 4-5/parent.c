8add77c4f34bae8d694182e7ce679e6bf4955c21
Nikita Annenkov <niannenkov@edu.hse.ru>
2024-04-24 23:08:49 +0300

    4-5

---
+++ 4-5/parent.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define TEXT_SIZE 100
+#define NUM_CHILDREN 5
+
+typedef struct {
+    char text[NUM_CHILDREN][TEXT_SIZE];
+    char encoded_texts[NUM_CHILDREN][TEXT_SIZE];
+    int encoded_count;
+    sem_t semaphore;
+} SharedData;
+
+SharedData *shared_data;
+
+void encode_text(int child_id) {
+    int i;
+    sem_wait(&shared_data->semaphore);
+    for (i = 0; shared_data->text[child_id][i] != '\0'; ++i) {
+        shared_data->encoded_texts[child_id][i] = (char)((int)shared_data->text[child_id][i] - 64);
+        
+    }
+    shared_data->encoded_texts[child_id][i] = '\0';
+
+    // Signal that this child has finished encoding its fragment
+    sem_post(&shared_data->semaphore);
+}
+
+void signal_handler(int signum) {
+    // Cleanup shared memory and semaphores
+    munmap(shared_data, sizeof(SharedData));
+    shm_unlink("/shared_memory");
+    sem_close(&shared_data->semaphore);
+    sem_unlink("/semaphore");
+    exit(signum);
+}
+
+int main() {
+    // Initialize shared memory
+    int shm_fd = shm_open("/shared_memory", O_CREAT | O_RDWR, 0666);
+    ftruncate(shm_fd, sizeof(SharedData));
+    shared_data = (SharedData *)mmap(NULL, sizeof(SharedData), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    // Initialize semaphore
+    sem_init(&shared_data->semaphore, 1, 0);
+
+    // Set up signal handler for proper cleanup
+    signal(SIGINT, signal_handler);
+
+
+    for (int i = 0; i < NUM_CHILDREN; i++) {
+        printf("Enter %d part of text to encode: ", i+1);
+        fgets(shared_data->text[i], TEXT_SIZE, stdin);
+        shared_data->text[i][strcspn(shared_data->text[i], "\n")] = '\0'; // Remove newline character
+    }
+    // Read input text
+   
+
+    // Fork child processes
+    int i;
+    pid_t pid;
+    for (i = 0; i < NUM_CHILDREN; ++i) {
+        pid = fork();
+        if (pid < 0) {
+            perror("fork");
+            exit(1);
+        } else if (pid == 0) { // Child process
+            encode_text(i);
+            exit(0);
+        }
+    }
+
+    // Parent process
+    // Wait for all children to finish encoding their fragments
+    for (i = 0; i < NUM_CHILDREN; ++i) {
+        wait(NULL);
+    }
+
+    // Print original text and encoded fragments
+    printf("Original text: ");
+    for (int i = 0; i < NUM_CHILDREN; ++i) {
+        printf("%s ", shared_data->text[i]);
+    }
+    printf("\n");
+    printf("Encoded fragments:\n");
+
+    for (int i = 0; i < NUM_CHILDREN; ++i) {
+        
+        printf("Fragment %d: %s\n", i+1, shared_data->encoded_texts[i]);
+        
+    }
+
+    printf("Encoded text: ");
+    for (int i = 0; i < NUM_CHILDREN; ++i) {
+        printf("%s ", shared_data->encoded_texts[i]);
+    }
+    printf("\n");
+
+    // Cleanup shared memory and semaphores
+    munmap(shared_data, sizeof(SharedData));
+    shm_unlink("/shared_memory");
+    sem_close(&shared_data->semaphore);
+    sem_destroy("/semaphore");
+
+    return 0;
+}
